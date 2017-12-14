84ba1d5e7732bb56454923a9c0054fc1a0003616
Nick Riddell <nickjriddell@gmail.com>
2017-12-14 19:24:56 +0000

    static ui

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
+        <div className="Brew-App">
+          <form>
+            <input type="text"/>
+          </form>
+          <div className="Brew-List">
+            <ul>
+              <li>
+                <input type="checkbox" /> Raspberry Mead
+              </li>
+              <li>
+                <input type="checkbox" /> Chcolate Mead
+              </li>
+              <li>
+                <input type="checkbox" /> Mead
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
